75fbc3cc0f6fce733a14ead707fc7099b14a7901
klnag <mohammedalasam815@gmail.com>
2023-02-18 18:52:04 +0000

    refactor 5

---
+++ moving.js
@@ -0,0 +1,32 @@
+let d = false
+
+function onMouseMove(event, s, initialX, initialY, transformMatrix) {
+    if(d) {
+        const dx = event.clientX - initialX;
+        const dy = event.clientY - initialY;
+        const newMatrix = transformMatrix.translate(dx, dy);
+        s.style.transform = newMatrix.toString();
+    }
+}
+
+function onMouseUp(event) {
+    d = false
+  document.removeEventListener("mousemove", onMouseMove);
+  document.removeEventListener("mouseup", onMouseUp);
+}
+
+export const moving = (e) => {
+    d = true
+  if (e.target.className === "mov") {
+    const s = e.target;
+    const transformMatrix = new DOMMatrix(getComputedStyle(s).transform);
+    let initialX = e.clientX;
+    let initialY = e.clientY;
+
+    document.addEventListener("mousemove", (event) => {
+      onMouseMove(event, s, initialX, initialY, transformMatrix);
+    });
+
+    document.addEventListener("mouseup", onMouseUp);
+  }
+}
--- script.js
+++ script.js
@@ -3,6 +3,8 @@ import {lbf} from "./lb.js";
 import {lrf} from "./lr.js";
 import {llf} from "./ll.js";
 
+import {moving} from "./moving.js";
+
 const lb = document.getElementById("lb");
 const lt = document.getElementById("lt");
 const lr = document.getElementById("lr");
@@ -10,40 +12,9 @@ const ll = document.getElementById("ll");
 
 
 const h1 = document.getElementById("h1_0");
-let d = false
 
-function onMouseMove(event, s, initialX, initialY, transformMatrix) {
-    if(d) {
 
-        
-        const dx = event.clientX - initialX;
-        const dy = event.clientY - initialY;
-        const newMatrix = transformMatrix.translate(dx, dy);
-        s.style.transform = newMatrix.toString();
-    }
-}
-
-function onMouseUp(event) {
-    d = false
-  document.removeEventListener("mousemove", onMouseMove);
-  document.removeEventListener("mouseup", onMouseUp);
-}
-
-h1.addEventListener("mousedown", (e) => {
-    d = true
-  if (e.target.className === "mov") {
-    const s = e.target;
-    const transformMatrix = new DOMMatrix(getComputedStyle(s).transform);
-    let initialX = e.clientX;
-    let initialY = e.clientY;
-
-    document.addEventListener("mousemove", (event) => {
-      onMouseMove(event, s, initialX, initialY, transformMatrix);
-    });
-
-    document.addEventListener("mouseup", onMouseUp);
-  }
-});
+h1.addEventListener("mousedown", moving);
 
 
 
