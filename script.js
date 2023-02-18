f73a2f97998a227a6cb05e35b5f53a2f54b48246
klnag <mohammedalasam815@gmail.com>
2023-02-18 17:24:35 +0000

    4

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const s = document.getElementById("s");
 let x = 0
 let xx = 0
 let v = false
+s.style.transform = `translate(0px, 0px)`
 console.log(x)
 lt.addEventListener("mousedown", function(event) {
   
@@ -16,14 +17,21 @@ lt.addEventListener("mousedown", function(event) {
   const startHeight = parseInt(getComputedStyle(s).height, 10);
   const startY = event.clientY;
 
-  x = Number(s.style.transform.slice(0,-3).slice(11))
+  // x = Number(s.style.transform.slice(10).split(", ")[1].slice(0,-3))
+  const match = s.style.transform.match(/translate\((-?\d+)px,\s*(-?\d+)px\)/);
+  console.log(s.style.transform)
+  x= match[2]
   function onMouseMove(event) {
-    
+    // console.log(s.style.transform.slice(0, -8).slice(11))
+    // const tx = match[1]
+    // const ty = match[2]
+
     const diffY = event.clientY - startY;
     const newHeight = startHeight - diffY;
-    // console.log(`${startHeight} + ${diffY} = ` + (startHeight + diffY) ,x)
+    console.log(match[1],Number(x)+diffY)
     s.style.height = newHeight + "px";
-    s.style.transform = `translateY(${x+diffY}px)`
+    // console.log(s.style.transform.slice(10).split(", "))
+    s.style.transform = `translate(${match[1]}px, ${Number(x)+diffY}px)`
     // childe.style.height = newHeight + "px";
     // childe.style.transform = `translateY(${x+diffY}px)`
     // childe.style.height = newHeight + "px";
@@ -99,14 +107,18 @@ ll.addEventListener("mousedown", function(event) {
 
   const startHeight = parseInt(getComputedStyle(s).width, 10);
   const startY = event.clientX;
+  // s.style.transform = `translate(0px, 0px)`
 
-  xx = Number(s.style.transform.slice(0,-3).slice(11))
+  const match1 = s.style.transform.match(/translate\((-?\d+)px,\s*(-?\d+)px\)/);
+  xx = match1[1]
   function onMouseMove(event) {
+    const match = s.style.transform.match(/translate\((-?\d+)px,\s*(-?\d+)px\)/);
     
     const diffY = event.clientX - startY;
+    console.log(Number(xx)+diffY)
     const newHeight = startHeight - diffY;
     s.style.width = newHeight + "px";
-    s.style.transform = `translateX(${xx+diffY}px)`
+    s.style.transform = `translate(${Number(xx)+diffY}px, ${match[2]}px)`
 
   }
   function onMouseUp() {
