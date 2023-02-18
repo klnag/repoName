9c84cff6687047c65382eac116189b3dde1ce942
klnag <mohammedalasam815@gmail.com>
2023-02-18 18:48:27 +0000

    add movable

--- script.js
+++ script.js
@@ -8,30 +8,44 @@ const lt = document.getElementById("lt");
 const lr = document.getElementById("lr");
 const ll = document.getElementById("ll");
 
+
 const h1 = document.getElementById("h1_0");
+let d = false
+
+function onMouseMove(event, s, initialX, initialY, transformMatrix) {
+    if(d) {
+
+        
+        const dx = event.clientX - initialX;
+        const dy = event.clientY - initialY;
+        const newMatrix = transformMatrix.translate(dx, dy);
+        s.style.transform = newMatrix.toString();
+    }
+}
+
+function onMouseUp(event) {
+    d = false
+  document.removeEventListener("mousemove", onMouseMove);
+  document.removeEventListener("mouseup", onMouseUp);
+}
 
 h1.addEventListener("mousedown", (e) => {
+    d = true
+  if (e.target.className === "mov") {
+    const s = e.target;
+    const transformMatrix = new DOMMatrix(getComputedStyle(s).transform);
+    let initialX = e.clientX;
+    let initialY = e.clientY;
 
-    if(e.target.className === "mov") {
-        const s = e.target
-        if(!s.style.transform) s.style.transform = `translate(0px, 0px)`
+    document.addEventListener("mousemove", (event) => {
+      onMouseMove(event, s, initialX, initialY, transformMatrix);
+    });
+
+    document.addEventListener("mouseup", onMouseUp);
+  }
+});
 
 
-        function onMouseMove(event) {
-            // s.style.transform = `translate(${}px, ${}px)`
-            console.log(s.style.transform)
-            console.log(event)
-        }
-        
-        function onMouseUp() {
-            document.removeEventListener("mousemove", onMouseMove);
-            document.removeEventListener("mouseup", onMouseUp);
-        }
-        
-        document.addEventListener("mousemove", onMouseMove);
-        document.addEventListener("mouseup", onMouseUp);
-    }
-})
 
 lt.addEventListener("mousedown", ltf);
 lb.addEventListener("mousedown", lbf);
